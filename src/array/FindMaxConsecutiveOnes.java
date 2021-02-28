7178a94ced24c4b7a409934c67d61172b519f1e5
Xu <1017390046@qq.com>
2021-02-28 23:00:04 +0800

    2021-2-28 23:00

---
+++ src/array/FindMaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+package array;
+
+public class FindMaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int [] nums={1,1,0,1,1,1};
+         System.out.println(findMaxConsecutiveOnes(nums));
+    }
+
+    public  static  int findMaxConsecutiveOnes(int[] nums) {
+         int max=0;
+         int count=0;
+         for(int i=0;i<nums.length;i++){
+             if(nums[i]==1){
+                 count++;
+                 if(count>max){
+                     max = count;
+                 }
+             }else{
+                 count=0;
+             }
+         }
+        return max;
+    }
+}
